6237ec98c4cb9e30f0140d75c59322a30ccee203
James Trotter <admin@floretos.com>
2017-04-12 16:09:58 -0400

    [beta] - fix spacing

--- Bootstrap.php
+++ Bootstrap.php
@@ -45,32 +45,32 @@ if(strpos($className, '\\') > 0) {
     $className = implode('/', $className);
 }
 if (file_exists(ROOT . DS . 'library' . DS . strtolower($className) . '.php')) {
-                require_once(ROOT . DS . 'library' . DS . strtolower($className) . '.php');
-            } else if (file_exists(ROOT . DS . 'Core' . DS . 'Helpers' . DS . $className . '.php')) {
-                require_once(ROOT . DS . 'Core' . DS . 'Helpers' . DS . $className . '.php');
-            } else if (file_exists(ROOT . DS . 'application' . DS . 'controllers' . DS . $className . '.php')) {
-                require_once(ROOT . DS . 'application' . DS . 'controllers' . DS . $className . '.php');
-            } else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php')) {
-                require_once(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php');
-            } else if (file_exists(ROOT . DS . 'Core' . DS . $className . '.php')) {
-                require_once(ROOT . DS . 'Core' . DS . $className . '.php');
-            } else if(file_exists(ROOT . DS . $className . '.php') && strpos($className, 'Helpers') == 0) {
-                require_once(ROOT . DS . $className . '.php');
-            } else if(substr($className, 0, 4) == "Core" && strpos($className, 'Helpers') > 0) {
-                if (file_exists(ROOT . DS . $className . '.php')) {
-                    require_once(ROOT . DS . $className . '.php');
-                } else {
-                   $name = explode('/', $className);
-                   $name = array_pop($name);
-                require_once(ROOT . DS . $className . DS . $name . '.php');
-                }
+        require_once(ROOT . DS . 'library' . DS . strtolower($className) . '.php');
+    } else if (file_exists(ROOT . DS . 'Core' . DS . 'Helpers' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'Core' . DS . 'Helpers' . DS . $className . '.php');
+    } else if (file_exists(ROOT . DS . 'application' . DS . 'controllers' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'application' . DS . 'controllers' . DS . $className . '.php');
+    } else if (file_exists(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'application' . DS . 'models' . DS . $className . '.php');
+    } else if (file_exists(ROOT . DS . 'Core' . DS . $className . '.php')) {
+        require_once(ROOT . DS . 'Core' . DS . $className . '.php');
+    } else if(file_exists(ROOT . DS . $className . '.php') && strpos($className, 'Helpers') == 0) {
+        require_once(ROOT . DS . $className . '.php');
+    } else if(substr($className, 0, 4) == "Core" && strpos($className, 'Helpers') > 0) {
+        if (file_exists(ROOT . DS . $className . '.php')) {
+            require_once(ROOT . DS . $className . '.php');
+        } else {
+           $name = explode('/', $className);
+           $name = array_pop($name);
+        require_once(ROOT . DS . $className . DS . $name . '.php');
+        }
 
-            } else {
-                //echo "</br>error loading files</br>Classname: ".$className;
-                //die("error autoloading files");
-                return false;
+    } else {
+        //echo "</br>error loading files</br>Classname: ".$className;
+        //die("error autoloading files");
+        return false;
 
-            }
+    }
 });
 
 
